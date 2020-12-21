b4f934b70e688c52bf217ad908ea870caa40bf64
oaygnehc <cyano9623@gmail.com>
2020-12-21 18:03:43 +0800

    prediction

--- predict.py
+++ predict.py
@@ -1,162 +1,204 @@
 #!/usr/bin/env python
-from __future__ import print_function
-import os,sys
-import pickle
+# coding: utf-8
+
 import keras
-from keras import backend as K
+import sys,os
+import h5py
 from keras.models import load_model
+import pickle
 import numpy as np
-import requests
-from networkx import nx
-from utils import *
+import networkx as nx
+from graphviz import Graph as DOTGraph
 from collections import Counter
-################
-import warnings
-warnings.filterwarnings("ignore")
-################
-try:
-    input_ID = str(sys.argv[1])
-except:
-    print("please input ID number")
-    sys.exit()
-coevolution_dict=dict()
-contact_handle = requests.get('https://gremlin2.bakerlab.org/preds_cst.php?db=SUB&id='+input_ID)
-for line in contact_handle.text.split("\n")[1:]:
-	line_list = line.split()
-	if len(line_list) == 9:
-		i_id = line_list[0]
-		j_id = line_list[1]
-		i_res = line_list[2]
-		j_res = line_list[3]
-		pair = i_res + j_res
-		if pair in CHED_pair:
-			coevolution_dict[(i_id,j_id)]=np.zeros((21,21))
-seq_num=0
-# function for parsing FASTA file
-def parse_fasta(lines):
-	'''function to parse fasta'''
-	header = []
-	sequence = []
-	for line in lines:
-		if len(line) > 0:
-			if line[0] == ">":
-				header.append(line[1:])
-				sequence.append([])
-			else:
-				sequence[-1].append(line)
-	sequence = [''.join(seq) for seq in sequence]
-	return header, sequence
 
-msa_handle = requests.get('http://openseq.org/sub_fasta.php?id='+input_ID)
-msa_lines = msa_handle.text.split("\n")
-headers, sequences = parse_fasta(msa_lines)
+model=load_model('database/dense_model_balanced8314.h5')
 
-origin_seq = sequences[0]
-for sequence in sequences:
-	sequence=list(sequence)
-	seq_num+=1
-	for pair in coevolution_dict.keys():
-		i_id=pair[0]
-		j_id=pair[1]
-		i_res=sequence[int(i_id)-1]
-		j_res=sequence[int(j_id)-1]
-		index_i=AA_dict[i_res]
-		index_j=AA_dict[j_res]
-		coevolution_dict[pair][index_i,index_j]+=1
-seq_len=len(sequence)
+
+alphabet='CHDENSTKGQYLAVRIMFWP-'
+
+states = len(alphabet)
+a2n = {}
+for a,n in zip(alphabet,range(states)):
+    a2n[a] = n
+
+def aa2num(aa):
+    if aa in a2n: return a2n[aa]
+    else: return a2n['-']
+    
+
+def parse_aln2seqmtx(filename,limit=-1):
+    sequence = []
+    f = open(filename, "r")
+    for line in f.readlines():
+        if line[0]=='>':
+            continue
+        line = line.strip()
+        sequence.append(line)
+    f.close()
+    return np.array(sequence)
+
+def frequency_matrix(seqs_mtx,i,j):
+    fq_mtx=np.zeros((21,21))
+    i=int(i)-1
+    j=int(j)-1
+    for r in range(seqs_mtx.shape[0]):   
+        iaa=seqs_mtx[r][i]
+        jaa=seqs_mtx[r][j]
+        fq_mtx[aa2num(iaa),aa2num(jaa)]+=1
+    return fq_mtx
+
+def get_frequency_mtx(msa_file,i,j):
+    fas_path=msa_file
+    m=parse_aln2seqmtx(fas_path)
+    fq_mtx=frequency_matrix(m,i,j)
+    fq_mtx=fq_mtx/np.sum(fq_mtx)
+    return fq_mtx
+
+
+
+def main(gene,msa_file,contact_file):
+    if os.path.getsize(msa_file) ==0:
+        print( gene,'blank msafile')
+        return
+    with open(msa_file,'r') as f:
+        if f.readline()[0]!= '>':
+            print(gene,'no msa')
+            return
+    CHED_pair=['HH','HC','CH','CC','HD','DD','DH','CD','DC','EE','EC','CE','DE','ED','HE','EH']
+    coevolution=[]
+    state=True
+    contact_handle=open(contact_file,'r')
+    for line in contact_handle.readlines():
+        line=line.strip()
+        if len(line)==0:
+            state=False
+            break
+        i_id=line.split()[-2][1:]
+        j_id=line.split()[-1][1:]
+        i_res=line.split()[-2][0]
+        j_res=line.split()[-1][0]
+        
+        pair=i_res+j_res
+        if pair in CHED_pair:
+            coevolution.append((i_id+'_'+i_res,j_id+'_'+j_res))
+    if state==False:
+        print( gene,'break confile')
+        return
+    print(coevolution)
 #########################predict the frequency matrix of each pair
-states = 21
-model = load_model('database/contact_model.h5')
-Protein=PROTEIN()
-for pair in coevolution_dict.keys():
-	data1=coevolution_dict[pair]/seq_num
-	data1=transform_matrix(data1)
-	x_prediction = data1.reshape(1, states, states, 1)
-	classes = model.predict(x_prediction,batch_size=1)
-	i_id=pair[0]
-	j_id=pair[1]
-	i_tag=i_id+"_"+origin_seq[int(i_id)-1]
-	j_tag=j_id+"_"+origin_seq[int(j_id)-1]
-	for i,num in enumerate(classes.argmax(axis=1)):
-		if num==1:
-			# print(i_tag,j_tag,num,classes[i][num])
-			Protein.metal.append((i_tag,j_tag))
-			if i_tag not in Protein.site:
-				Protein.site.append(i_tag)
-			if j_tag not in Protein.site:
-				Protein.site.append(j_tag)
-			Protein.prob_dict[(i_tag,j_tag)]=classes[i][num]
+    G=nx.Graph()
+    output_file='%s.cnn'%gene 
+    output_handle=open(output_file,'w')
+    for pair in coevolution:
+        i_AA=pair[0]
+        j_AA=pair[1]
+        i=pair[0].split('_')[0]
+        j=pair[1].split('_')[0]
+        data=get_frequency_mtx(msa_file,i,j)
+        x_prediction = data.reshape(1,441)  
+        weighted_prediction = model.predict(x_prediction,batch_size=1)
+        pred = weighted_prediction[:,1]
+        if pred > 0.5:
+            G.add_edge(i_AA,j_AA)
+        output_handle.write(i_AA+"\t"+j_AA+"\t"+str(pred)+"\n")        
+    output_handle.close()
+    nx.write_gpickle(G,'%s.gpickle'%gene )
 
 
-##################################################
-# to load the 2D bank information
-###############################################
-bench_file="database/benchmark.pkl"
-if os.path.exists(bench_file):
-	f2=file(bench_file,'rb')
-	bench_r_dict=pickle.load(f2)
-	f2.close()
-else:
-    print("benchmark database file missing")
 
-#########################################
-#to separate each subgraph as output 
-# and to predict the metal type
-#########################################
-flag=0
-dot = Graph(comment='The Round Table',format='png')
-new_matrix=list2mtx(Protein.site,Protein.metal)
-for (i,mtx) in enumerate(subgraph(new_matrix)):
-    mtx=find_ring(mtx)
-    pair_list=mtx2list(Protein.site,mtx)
-    mtx_site=node_count(pair_list)
-    if len(mtx_site)<=3:
-        continue
-    if find_ring(mtx,False):
-    	temp_str=str()
-    	for line in pair_list:
-            for a in line:
-    	        temp_str=temp_str+" "+a
-    	        temp_str=temp_str+";"
-    	#### set a graph G for comparison with 2D bank
-    	G1=nx.Graph()
-    	for pair in pair_list:
-    	    i_tag=pair[0]
-    	    j_tag=pair[1]
-    	    i_id=i_tag.split("_")[0]+i_tag.split("_")[1]
-    	    j_id=j_tag.split("_")[0]+j_tag.split("_")[1]
-    	    i=int(i_tag[0:-2])
-    	    j=int(j_tag[0:-2])
-    	    if fabs(i-j) <=3:
-    	    	gap_type='N'
-    	    else:
-    	    	gap_type='F'
-    	    #####trying to read the probability with wrong order
-    	    try:
-    	    	prob=Protein.prob_dict[pair]				
-    	    except:
-    	    	prob=Protein.prob_dict[(j_tag,i_tag)]
-    	    ##### adding nodes and edges in the G graph
-    	    G1.add_node(i_id,residue=i_tag.split("_")[1])
-    	    G1.add_node(j_id,residue=j_tag.split("_")[1])
-    	    G1.add_edge(i_id,j_id,gap_type=gap_type)
-            dot.node(i_id,style="radial",fillcolor=color_def(i_tag.split("_")[1]))
-            dot.node(j_id,style="radial",fillcolor=color_def(j_tag.split("_")[1]))
-            dot.edge(i_id,j_id,penwidth="2",color=color_digit(float(prob)*2-1))
-    	    flag=1
-        pred_metal=list()
-    	#########transfer the metal type from the same graph
-    	for bench_G in bench_r_dict.keys():
-    	    GM=nx.isomorphism.GraphMatcher(bench_G,G1,node_match=nx.isomorphism.categorical_node_match(['residue','residue','residue','residue'],['C','D','E','H']),edge_match=nx.isomorphism.categorical_edge_match(['gap_type','gap_type'],['N','F']))
-            if G1.number_of_nodes() == bench_G.number_of_nodes():
-                if GM.is_isomorphic() or GM.subgraph_is_isomorphic():
-    	            pred_metal=bench_r_dict[bench_G]
-    	            break
-    	result = []
-    	if len(pred_metal)>0:
-            result.append(str(pred_metal[0]))
-            dot.edge(str(pred_metal[0]),j_id,penwidth="0",color="white")
-    	else:
-    	    result.append(str(pred_metal))
-if flag >0:
-    dot.render(input_ID, view=False)
+
+def color_def(elem):
+    if elem=="C":
+        color="#FFED97"
+    elif elem=="H":
+        color="#66B3FF"
+    elif elem=="E":
+        color="#FFA6FF"
+    elif elem =="D":
+        color="#D3A4FF"
+    return color
+
+def find_ring(subG):
+    g=nx.Graph(subG)
+    while ([d<2 for (n,d) in list(g.degree())].count(True)>0):
+        for (n,d) in list(g.degree()):
+            if d<2:
+                g.remove_node(n)
+    return g
+
+def resi_comparison(G1,G2):
+    return G1['residue']==G2['residue']
+def gap_comparison(G1,G2):
+    return G1['gap_label']==G2['gap_label']
+
+
+
+
+def motif_scan(G1,stdout):
+    f=open('database/motif_bank.pkl','rb')
+    motif_bank=pickle.load(f)
+    f.close()
+    result=[]
+    for subG_metal in list(motif_bank.keys()):
+        graph_list=motif_bank[subG_metal]
+        for count,G2 in enumerate(graph_list):
+            GM = nx.isomorphism.GraphMatcher(G1,G2,node_match=resi_comparison, edge_match=gap_comparison)
+            if GM.is_isomorphic():  
+                result.append(subG_metal)
+                print(subG_metal,count,file=stdout)
+                print(GM.mapping,file=stdout)
+    result=dict(Counter(result))
+    for k in list(result.keys()):
+        ratio="%.2f"%(result[k]/len(motif_bank[k]))
+        print(k,result[k],len(motif_bank[k]),ratio,file=stdout)
+
+
+
+
+def option(gene):
+    G=nx.read_gpickle('%s.gpickle'%gene)
+    dot = DOTGraph(comment=gene,format='eps')
+    GT_export=open("%s.dat"%gene,'w')
+    for c in nx.connected_components(G):
+        nodeset=G.subgraph(c).nodes()
+        if len(nodeset)<3:          
+            continue
+        subG=nx.Graph(G.subgraph(c))
+        subG=find_ring(subG)
+        pair_list = list(subG.edges)
+        print(gene,pair_list,file=GT_export)
+        if len(pair_list) >=2:      
+            for pair in pair_list:        
+                i_tag,iAA=pair[0].split('_')
+                j_tag,jAA=pair[1].split('_')
+                i_id=i_tag+iAA            
+                j_id=j_tag+jAA
+                attrs={pair[0]:{'residue':iAA,'id':i_id},pair[1]:{'residue':jAA,'id':j_id}}
+                nx.set_node_attributes(subG,attrs)
+                gap=abs(int(i_tag)-int(j_tag))
+                attrs={(pair[0],pair[1]):{'gap':gap,'gap_label': (gap<3)}}
+                nx.set_edge_attributes(subG,attrs) 
+                ##draw graph
+                dot.node(str(i_id),style="radial",fillcolor=color_def(iAA))
+                dot.node(str(j_id),style="radial",fillcolor=color_def(jAA))
+                if gap < 3:
+                    penwidth= '2'
+                else:
+                    penwidth= '1'
+                dot.edge(str(i_id),str(j_id),penwidth=penwidth)
+            motif_scan(subG,GT_export)
+    dot.render('%s.gv'%gene,view=False)
+    GT_export.close()
+
+
+
+
+
+gene=sys.argv[1]
+msa_file='%s.msa'%gene
+contact_file='%s.copair'%gene
+main(gene,msa_file,contact_file)
+if os.path.exists("%s.gpickle"%gene):
+    if os.path.getsize("%s.gpickle"%gene) >0:
+        option(gene)
+
